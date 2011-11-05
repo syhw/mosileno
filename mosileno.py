7f1aaff1db062dadd06a5498c47bf61411dc1b16
syhw <gabriel.synnaeve@gmail.com>
2011-11-05 21:49:19 +0100

    Timers stuff (callbacks JS/routes/DB)

--- mosileno.py
+++ mosileno.py
@@ -4,8 +4,11 @@ from urllib import quote, unquote
 
 from pyramid.config import Configurator
 from pyramid.session import UnencryptedCookieSessionFactoryConfig
-
 from pyramid.view import view_config
+from pyramid.response import Response
+
+from mongokit import Connection
+from timer import Timer
 
 from paste.httpserver import serve
 
@@ -14,16 +17,12 @@ log = logging.getLogger(__file__)
 
 here = os.path.dirname(os.path.abspath(__file__))
 
-@view_config(route_name='frame', renderer='frame.mako')
-def frame(request):
-    return {'url': unquote(request.matchdict['url'])}
-
 @view_config(route_name='root', renderer='hn.mako')
 def root(request):
     #items = []
-    items = [{#{'url': "http://www.yahoo.fr",
-        'url': request.route_url('frame', 
-            url=quote("http://www.yahoo.fr", safe="")),
+    items = [{'url': "http://www.yahoo.fr",
+        #'url': request.route_url('frame', 
+        #    url=quote("http://www.yahoo.fr", safe="")),
         'domain': "yahoo.com",
         'title': "Yahoo",
         'points': 666,
@@ -32,21 +31,55 @@ def root(request):
         'num_comments': 69}]
     return {'items': items}
 
+@view_config(route_name='frame', renderer='frame.mako')
+def frame(request):
+    return {'url': unquote(request.GET.get('url'))}
+
+@view_config(route_name='timer_start')
+def timer_start(request):
+    t = request.registry.settings['db'].hdparis114.timers.Timer(request.matchdict['user'],
+            request.GET.get('url'))
+    t.save()
+    return Response(t._id)
+
+@view_config(route_name='timer_restart')
+def timer_restart(request):
+    t = request.registry.settings['db'].Timer.find({'_id': 
+            request.matchdict['id']})
+    t.start(request.matchdict['time'])
+    t.save()
+    return Response("OK")
+
+@view_config(route_name='timer_stop')
+def timer_stop(request):
+    t = request.registry.settings['db'].Timer.find({'_id': 
+            request.matchdict['id']})
+    t.stop()
+    t.save()
+    return Response("OK")
+
 if __name__ == '__main__':
     # configuration settings
     settings = {}
     settings['reload_all'] = True
     settings['debug_all'] = True
+    settings['debug_templates'] = True
+    settings['debug_routematch'] = True
+    settings['includes'] = ["pyramid_debugtoolbar"]
     settings['mako.directories'] = os.path.join(here, 'templates')
+    con = Connection('mongodb://hdparis114:f41922d68004e@hackday.mongohq.com:27017/hdparis114')
+    con.register([Timer])
+    settings['db'] = con
     # session factory
     session_factory = UnencryptedCookieSessionFactoryConfig('secret')
     # configuration setup
     config = Configurator(settings=settings, session_factory=session_factory)
+    config.add_static_view('static', 'static/')
     config.add_route('root', '/')
-    config.add_route('frame', '/frame/{url}')
-#    config.add_route('timer_start', '/timerstart/{time}')
-#    config.add_route('timer_restart', '/timerrestart/{id}/{time}')
-#    config.add_route('timer_stop', '/timerstop/{id}')
+    config.add_route('frame', '/frame')
+    config.add_route('timer_start', '/timer_start/{user}')
+    config.add_route('timer_restart', '/timer_restart/{id}')
+    config.add_route('timer_stop', '/timer_stop/{id}')
     config.scan()
     # serve app
     app = config.make_wsgi_app()
--- templates/frame.mako
+++ templates/frame.mako
@@ -2,6 +2,32 @@
 <!DOCTYPE html>
 
 <head>
+<script src="static/jquery-1.7.min.js"></script>
+<script>
+var count_loads = 0;
+var timer_id = null; 
+function timer_start() 
+{ 
+console.log('start');/////////////////
+    $.get('timer_start/test_user?url=${url | u}', function(data)
+            { 
+console.log('startCB');/////////////////
+            timer_id = data;
+            return false;
+            }
+         );
+console.log('end'); /////////////////
+};
+timer_start();
+function timer_stop() 
+{ 
+    if (count_loads > 0)
+        $.get('timer_stop/' + '${id}');
+    else
+        count_loads++;
+};
+$(document).bind(onbeforeunload, timer_stop);
+</script>
 <meta charset="utf-8">
         <style type="text/css"> 
             html {overflow: auto;} 
@@ -10,6 +36,6 @@
         </style> 
 </head>
 <body>
-        <iframe id="frame" name="frame" src="${url}" frameborder="0" marginheight="0" marginwidth="0" width="100%" height="100%" scrolling="auto" onLoad="alert('Test');">Your browser doesn't support iFrames... <a href="${url}">Here it is</a></iframe> 
+        <iframe id="frame" name="frame" src="${url}" frameborder="0" marginheight="0" marginwidth="0" width="100%" height="100%" scrolling="auto" onLoad="timer_stop();">Your browser doesn't support iFrames... <a href="${url}">Here it is</a></iframe> 
 </body>
 </html>
